def19a503b567b25c9c3a305d488cd1890a080cc
Nick Zuzow <zuzownic@gmail.com>
2025-01-10 11:16:30 -0500

    feat(FeaturedPosts): Created initial stub markup and layout for FeaturedPosts component. Added it to the HomePage

---
+++ src/components/FeaturedPosts.jsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+import Image from "./Image";
+
+const FeaturedPosts = () => {
+    return (
+        <div className="mt-8 flex flex-col lg:flex-row gap-8">
+            {/* First */}
+            <div className="w-full lg:w-1/2 flex flex-col gap-4">
+                {/* image */}
+                <Image src="featured1.jpeg" className="rounded-3xl object-cover" />
+                {/* details */}
+                <div className="">
+                    <span className="font-semibold lg:text-lg">01.</span>
+                    <Link className="text-blue-800 lg:text-lg">Web Design</Link>
+                    <span className="text-gray-500">2 days ago</span>
+                </div>
+                {/* title */}
+                <Link to="/test" className="text-xl lg:text-3xl font-semibold lg:font-bold">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Link>
+            </div>
+            {/* Other */}
+            <div className="w-full lg:w-1/2 flex flex-col gap-4">
+                {/* Second */}
+                <div className="lg:h-1/3 flex justify-between gap-4">
+                    <Image src="featured2.jpeg" className="rounded-3xl object-cover w-1/3 aspect-video" />
+                    <div className="w-2/3">
+                        {/* details */}
+                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                            <span className="font-semibold">02.</span>
+                            <Link className="text-blue-800">Web Design</Link>
+                            <span className="text-gray-500 text-sm">2 days ago</span>
+                        </div>
+                        {/* title */}
+                        <h2><Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Link></h2>
+                    </div>
+                </div>
+                {/* Third */}
+                <div className="lg:h-1/3 flex justify-between gap-4">
+                    <Image src="featured2.jpeg" className="rounded-3xl object-cover w-1/3 aspect-video" />
+                    <div className="w-2/3">
+                        {/* details */}
+                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                            <span className="font-semibold">02.</span>
+                            <Link className="text-blue-800">Web Design</Link>
+                            <span className="text-gray-500 text-sm">2 days ago</span>
+                        </div>
+                        {/* title */}
+                        <h2><Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Link></h2>
+                    </div>
+                </div>
+                {/* Fourth */}
+                <div className="lg:h-1/3 flex justify-between gap-4">
+                    <Image src="featured2.jpeg" className="rounded-3xl object-cover w-1/3 aspect-video" />
+                    <div className="w-2/3">
+                        {/* details */}
+                        <div className="flex items-center gap-4 text-sm lg:text-base mb-4">
+                            <span className="font-semibold">02.</span>
+                            <Link className="text-blue-800">Web Design</Link>
+                            <span className="text-gray-500 text-sm">2 days ago</span>
+                        </div>
+                        {/* title */}
+                        <h2><Link to="/test" className="text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Link></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedPosts;
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MainCategories from "../components/MainCategories";
+import FeaturedPosts from "../components/FeaturedPosts";
 
 const HomePage = () => {
     return (
@@ -59,6 +60,7 @@ const HomePage = () => {
             {/* CATEGORIES */}
             <MainCategories />
             {/* FEATURED POSTS */}
+            <FeaturedPosts />
             {/* POST LIST */}
         </div>
     );
