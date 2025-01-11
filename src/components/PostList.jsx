0a368420f41676b9674ea859800868920bf4baf3
Nick Zuzow <zuzownic@gmail.com>
2025-01-11 18:04:14 -0500

    feat(Posts): Created PostList and PostListItem components and stubbed out the markup. Updated the HomePage component to show the new PostList component

---
+++ src/components/PostList.jsx
@@ -0,0 +1,11 @@
+import PostListItem from "./PostListItem";
+
+const PostList = () => {
+    return (
+        <div className="flex flex-col gap-12 mb-8">
+            <PostListItem />
+        </div>
+    );
+};
+
+export default PostList;
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import MainCategories from "../components/MainCategories";
 import FeaturedPosts from "../components/FeaturedPosts";
+import PostList from "../components/PostList";
 
 const HomePage = () => {
     return (
@@ -62,6 +63,10 @@ const HomePage = () => {
             {/* FEATURED POSTS */}
             <FeaturedPosts />
             {/* POST LIST */}
+            <div className="">
+                <h2 className="my-8 text-2xl text-gray-600">Recent Posts</h2>
+                <PostList />
+            </div>
         </div>
     );
 };
