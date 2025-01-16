5e68596109fb7a6a67383730e025c67a5f9d4d13
Nick Zuzow <zuzownic@gmail.com>
2025-01-16 02:21:54 -0500

    feat(Comments): Added initial markup / styling for comments components used on the single post page

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,6 +1,23 @@
+import Image from "./Image";
+
 const Comment = () => {
     return (
-        <div className="">Comment</div>
+        <div className="p-4 bg-slate-50 rounded-xl">
+            <div className='flex items-center gap-4'>
+                <Image
+                    src="userImg.jpeg"
+                    className="w-10 h-10 rounded-full object-cover"
+                    w="40"
+                />
+                <span className="font-medium">John Doe</span>
+                <span className="text-sm text-gray-500">2 days ago</span>
+            </div>
+            <div className="mt-4">
+                <p>
+                    Lorem ipsum odor amet, consectetuer adipiscing elit. Tempor nostra torquent maximus quisque turpis neque duis lobortis. Neque magna risus id aptent; natoque pharetra. Phasellus inceptos dis fermentum viverra himenaeos vitae dapibus hendrerit.
+                </p>
+            </div>
+        </div>
     );
 };
 
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,6 +1,17 @@
+import Comment from "./Comment";
+
 const Comments = () => {
     return (
-        <div className="">Comments</div>
+        <div className="flex flex-col gap-8 lg:w-3/5 pb-8">
+            <h2 className="text-xl text-gray-600 underline">Comments</h2>
+            <div className="flex items-center justify-between gap-8 w-full">
+                <textarea placeholder="Write a comment..." className="w-full p-4 rounded-xl text-gray-600"/>
+                <button className="bg-blue-800 px-4 py-3 text-white font-medium rounded-xl">Send</button>
+            </div>
+            <Comment />
+            <Comment />
+            <Comment />
+        </div>
     );
 };
 
