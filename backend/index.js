70a0774d8bf04b012e487fca85943c99f4b30d3d
Nick Zuzow <zuzownic@gmail.com>
2025-01-17 12:45:35 -0500

    feat(backend): initial setup of backend app. Connected MongoDB using mongoose. Setup the model, routes, and controller for the Post related endpoints. Setup a model and stubbed out a test route for the User and Comment objects.

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import 'dotenv/config';
+import connectDB from "./lib/connectDB.js";
+import commentRouter from "./routes/comment.route.js";
+import postRouter from "./routes/post.route.js";
+import userRouter from "./routes/user.route.js";
+
+const PORT = parseInt(process.env.SERVER_PORT, 10) || 3000;
+const app = express();
+app.use(express.json());
+
+app.use("/comments", commentRouter);
+app.use("/posts", postRouter);
+app.use("/users", userRouter);
+
+// Error Handler
+app.use((err, req, res, next) => {
+    res.status(err.status || err.statusCode || 500);
+    res.json({
+        message: err.message || "something went wrong",
+        status: err.status || err.statusCode || 500,
+        stack: err.stack,
+    });
+});
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running! listening on: ", PORT);
+});
